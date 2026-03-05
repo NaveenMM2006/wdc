31ea5824959f99b5a8aeb1ad0c1fbb7ee7026f19
Naveen Mastamardi <mastamardinaveen524@gmail.com>
2026-03-05 14:17:30 +0530

    env prt added

--- src/config/db.js
+++ src/config/db.js
@@ -6,12 +6,16 @@ dotenv.config();
 
 const pool = mysql2.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections:true,
     connectionLimit:10,
-    queueLimit:true,
+    queueLimit:0,
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
